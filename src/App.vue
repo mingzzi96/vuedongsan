aa8c9ca1de2c8aac8030f582354c7c8da86b4e54
Jimin Kwon <jimin23623@gmail.com>
2024-07-01 18:34:24 +0900

    feat: 모달창 만들어서 열고 닫기

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,18 @@
     <a v-for="(menu, i) in menus" :key="menu[i]">{{menu}}</a>
   </div>
 
+  <div class="black-bg" v-if="isOpen === true">
+    <div class="white-bg">
+      <h4>상세페이지</h4>
+      <p>상세페이지내용임</p>
+      <button @click="isOpen = false">닫기</button>
+    </div>
+  </div>
+
   <img alt="Vue logo" src="./assets/logo.png">
   <ul>
     <li v-for="(product, i) in products" :key="product.name+i" :id="product.name+i">
-      <h4>{{product.name}}</h4>
+      <h4 @click="isOpen = true">{{product.name}}</h4>
       <p>{{product.price}}만원</p>
       <div>
         <button @click="increase(i)">허위매물 신고</button>
@@ -22,7 +30,8 @@ export default {
   name: 'App',
   data(){
     return {
-      menus: ['Home', 'Product', 'Mypage'],
+      isOpen: false,
+      menus: ['Home', 'Product', 'MyPage'],
       products : [{name:'역삼동원룸', price: 46, count: 0}, {name:'천호동원룸', price:50, count: 0}, {name:'마포구원룸', price: 60, count: 0}],
     }
   },
@@ -54,4 +63,20 @@ export default {
   color : white;
   padding : 10px;
 }
+body {
+  margin : 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height:100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+} 
 </style>
